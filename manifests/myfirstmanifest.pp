51c56811f556a933c8c22ee7497aa536c74b4e8e
Chris Bale <chris.bale@aria-networks.com>
2018-10-12 13:37:04 +0100

    Automation testing

--- manifests/myfirstmanifest.pp
+++ manifests/myfirstmanifest.pp
@@ -1,4 +1,4 @@
 file { '/tmp/hudson.txt':
 	ensure => file,
-	content => "NAME=Hudson_Bale_Burgos, DOB=(11.12.12), CHARS=[Loyal, Happy, Intelligent, Loving, Handsome, Strong, Virtuous, Couragous, Sensitive, Tenacious, Compassionate]"
+	content => "NAME=Hudson_Bale_Burgos, DOB=(11.12.12), CHARS=[Inquisitive, Loyal, Happy, Intelligent, Loving, Handsome, Strong, Virtuous, Couragous, Sensitive, Tenacious, Compassionate]"
 }
