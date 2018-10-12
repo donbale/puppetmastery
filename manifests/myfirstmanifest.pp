3e300b38ddd32ba3e775cc8f70f7dc411ad73411
Chris Bale <chris.bale@aria-networks.com>
2018-10-12 11:33:55 +0100

    Remember your permission

--- manifests/myfirstmanifest.pp
+++ manifests/myfirstmanifest.pp
@@ -1,4 +1,4 @@
 file { '/tmp/hudson.txt':
 	ensure => file,
-	content => "NAME=Hudson_Bale_Burgos, DOB=(11.12.12), CHARS=[Intelligent, Loving, Handsome, Strong, Virtuous, Couragous, Sensitive, Compassionate]"
+	content => "NAME=Hudson_Bale_Burgos, DOB=(11.12.12), CHARS=[Intelligent, Loving, Handsome, Strong, Virtuous, Couragous, Sensitive, Tenacious, Compassionate]"
 }
