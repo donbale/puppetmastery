4ea6ac6f5c13d9e9b26c09fe5b4cf4eafcb8726f
Chris Bale <chris.bale@aria-networks.com>
2018-10-12 09:04:04 +0100

    Testing vmhost2 automation

--- manifests/myfirstmanifest.pp
+++ manifests/myfirstmanifest.pp
@@ -1,4 +1,4 @@
 file { '/tmp/hudson.txt':
 	ensure => file,
-	content => "NAME=Hudson_Bale_Burgos, DOB=(11.12.12), CHARS=[Highly Intelligent, Loving, Handsome, Strong, Virtuous, Couragous]"
+	content => "NAME=Hudson_Bale_Burgos, DOB=(11.12.12), CHARS=[Highly Intelligent, Loving, Handsome, Strong, Virtuous, Couragous, Sensitive]"
 }
