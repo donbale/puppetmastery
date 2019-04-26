5c8869f3d87cfcdb256f0228cf13a589f2bf6229
Chris Bale <christopherbale@icloud.com>
2019-04-26 10:58:31 +0100

    Removed ownership from manifest

--- manifests/powerchute.pp
+++ manifests/powerchute.pp
@@ -1,23 +1,17 @@
 file { '/tmp/jre-11.0.1_linux-x64_bin.tar.gz':
   ensure => file,
-  owner  => 'administrator',
-  group  => 'administrator',
   mode   => '0777',
   source => '/etc/puppetlabs/code/environments/production/files/powerchute/jre-11.0.1_linux-x64_bin.tar.gz',
 }
 
 file { '/tmp/pcns430.tar.gz':
   ensure => file,
-  owner  => 'administrator',
-  group  => 'administrator',
   mode   => '0777',
   source => '/etc/puppetlabs/code/environments/production/files/powerchute/pcns430.tar.gz',
 }
 
 file { '/tmp/install.sh':
   ensure => file,
-  owner  => 'root',
-  group  => 'root',
   mode   => '0777',
   source => '/etc/puppetlabs/code/environments/production/files/powerchute/install.sh',
 }
