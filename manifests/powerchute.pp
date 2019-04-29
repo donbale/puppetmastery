0b03a15afc568bce43aef5c81e2b18348e4aac32
Chris Bale <christopherbale@icloud.com>
2019-04-29 08:02:43 +0100

    Changed mkdir exec to ensure directory exists in puppet style

--- manifests/powerchute.pp
+++ manifests/powerchute.pp
@@ -1,22 +1,22 @@
-exec { 'make_directory_powerchute':
-  command => 'mkdir -p /home/administrator/Linux_64',
-  path    => '/usr/local/bin/:/bin/',
+file { '/home/administrator/Linux_64':
+  ensure => directory,
+  mode   => '0755',
 }
 
 file { '/home/administrator/Linux_64/jre-11.0.1_linux-x64_bin.tar.gz':
   ensure => file,
-  mode   => '0777',
+  mode   => '0755',
   source => '/etc/puppetlabs/code/environments/production/files/powerchute/jre-11.0.1_linux-x64_bin.tar.gz',
 }
 
 file { '/home/administrator/Linux_64/pcns430.tar.gz':
   ensure => file,
-  mode   => '0777',
+  mode   => '0755',
   source => '/etc/puppetlabs/code/environments/production/files/powerchute/pcns430.tar.gz',
 }
 
 file { '/home/administrator/Linux_64/install.sh':
   ensure => file,
-  mode   => '0777',
+  mode   => '0755',
   source => '/etc/puppetlabs/code/environments/production/files/powerchute/install.sh',
 }
