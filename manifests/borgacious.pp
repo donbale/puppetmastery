b8d019b11a04a4d610b57e479f99243ca875d6c4
Chris Bale <christopherbale@icloud.com>
2019-05-14 09:31:34 +0100

    Added cronjob to borgacious manifest ensuring borg backup runs on vms 22:00 todo noche

--- manifests/borgacious.pp
+++ manifests/borgacious.pp
@@ -9,14 +9,22 @@ file { '/etc/block-fuse':
   source => '/etc/puppetlabs/code/environments/production/files/borgacious/block-fuse',
 }
 
-file { '/home/administrator/borgacious.sh':
+file { '/usr/local/bin/borgacious.sh':
   ensure => file,
   mode   => '0755',
   source => '/etc/puppetlabs/code/environments/production/files/borgacious/borgacious.sh',
 }
 
-file { '/home/administrator/recoverone.sh':
+file { '/usr/local/bin/recoverone.sh':
   ensure => file,
   mode   => '0755',
   source => '/etc/puppetlabs/code/environments/production/files/borgacious/recoverone.sh',
+}
+
+cron { 'run-borgbackup':
+  ensure => 'present',
+  command => '/usr/local/bin/borgacious.sh >> /tmp/borgcron 2>&1',
+  user  => root,
+  hour    => '22',
+  minute  => '0',
 }
